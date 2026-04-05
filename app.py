d4a47677b6079625d9bb6e2d46fdf7e11d8b9425
ziadabdelwahed <zabdelwahed930@gmail.com>
2026-04-05 11:42:57 +0200

    Migrate mnemonic generation to Streamlit app
    Replace HTTP server with Streamlit interface for mnemonic generation.

--- app.py
+++ app.py
@@ -1,22 +1,10 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+import streamlit as st
 from main import MnemonicEngine
 
-class MyHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        result = MnemonicEngine.generate(128)
-        words = result.mnemonic.split()
+st.title("Mnemonic Generator 🔥")
 
-        html = "<h2>Mnemonic Generator</h2>"
-        html += "<a href='/'>Generate New</a><br><br>"
+if st.button("Generate 12 Words"):
+    words = MnemonicEngine.generate(128).split()
 
-        for i, word in enumerate(words, 1):
-            html += f"{i}- {word}<br>"
-
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(html.encode())
-
-server = HTTPServer(("0.0.0.0", 8000), MyHandler)
-print("Server running on http://localhost:8000")
-server.serve_forever()
+    for i, word in enumerate(words[:12], 1):
+        st.write(f"{i}- {word}")
