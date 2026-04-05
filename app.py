4d432e8fdeda358ce822cef71e1a1791be99c03f
ziadabdelwahed <zabdelwahed930@gmail.com>
2026-04-05 12:36:46 +0200

    Refactor mnemonic generation logic in app.py

--- app.py
+++ app.py
@@ -3,11 +3,10 @@ from main import MnemonicEngine
 
 st.title("Mnemonic Generator 💻")
 
-engine = MnemonicEngine()  # 👈 دي أهم سطر
-
 if st.button("Generate 12 Words"):
-    words = engine.generate(128).split()
+    result = MnemonicEngine.generate(128)  # 👈 بيرجع object
+
+    words = result.mnemonic.split()  # 👈 الصح هنا
 
     for i, word in enumerate(words[:12], 1):
         st.write(f"{i}- {word}")
-
