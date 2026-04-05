5e2715df3aa213ae5d7cbd7b859c075be7583c56
ziadabdelwahed <zabdelwahed930@gmail.com>
2026-04-05 13:40:14 +0200

    Change button labels from Arabic to English

--- app.py
+++ app.py
@@ -8,19 +8,19 @@ col1, col2, col3, col4, col5 = st.columns(5)
 
 result = None
 
-if col1.button("12 كلمة"):
+if col1.button("12 words"):
     result = MnemonicEngine.generate(128)
 
-if col2.button("15 كلمة"):
+if col2.button("15 words"):
     result = MnemonicEngine.generate(160)
 
-if col3.button("18 كلمة"):
+if col3.button("18 words"):
     result = MnemonicEngine.generate(192)
 
-if col4.button("21 كلمة"):
+if col4.button("21 words"):
     result = MnemonicEngine.generate(224)
 
-if col5.button("24 كلمة"):
+if col5.button("24 words"):
     result = MnemonicEngine.generate(256)
 
 # 👇 عرض النتيجة
