d378ac76537623f3d7c76862aa6bd71e5cfc9c8f
ziadabdelwahed <zabdelwahed930@gmail.com>
2026-04-05 12:40:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ from main import MnemonicEngine
 st.title("Mnemonic Generator 💻")
 
 if st.button("Generate 12 Words"):
-    result = MnemonicEngine.generate(128)  # 👈 بيرجع object
+    result = MnemonicEngine.generate(128)
 
-    words = result.mnemonic.split()  # 👈 الصح هنا
+    mnemonic_text = str(result.mnemonic)  # 👈 نحوله نص غصب عنه
+
+    words = mnemonic_text.split()
 
     for i, word in enumerate(words[:12], 1):
         st.write(f"{i}- {word}")
